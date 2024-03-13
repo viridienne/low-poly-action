acec2079b3f7206a29060e99d504ec3db4ebbc5f
KinoZero <nminhquan143@gmail.com>
2024-03-13 23:53:50 +0700

    *add character rotation (temp)

--- low_poly_action/Assets/Script/ControlMovement.cs
+++ low_poly_action/Assets/Script/ControlMovement.cs
@@ -16,9 +16,13 @@ public class ControlMovement : MonoBehaviour
 
     public void Move(Vector2 _vector2)
     {
-      var _current = tf.position;
-      var _new = new Vector3(_current.x + _vector2.x, _current.y, _current.z + _vector2.y);
+        var _current = tf.position;
+        var _new = new Vector3(_current.x + _vector2.x, _current.y, _current.z + _vector2.y);
         tf.position = Vector3.Lerp(_current, _new, configMovement.walkSpeed * Time.deltaTime);
+        if (_vector2.x != 0 || _vector2.y != 0)
+        {
+            tf.rotation = Quaternion.Euler(0f, (float)(System.Math.Atan2((_vector2.x - 0), (_vector2.y - 0)) * 180 / 3.14), 0f);
+        }
     }
 
 }
